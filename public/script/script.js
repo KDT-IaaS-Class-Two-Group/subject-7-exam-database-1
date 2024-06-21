fcfc269a3f62dd31f8e299c6293cab754d5615cd
정호연 <jea7730@gmail.com>
2024-06-21 12:30:18 +0900

    startBu 이벤트 리스너 주석처리함, exitBu을 누르면 검정 화면이 나오게 하기

--- public/script/script.js
+++ public/script/script.js
@@ -5,8 +5,13 @@ const startBu = document.querySelector('#start');
 const exitBu = document.querySelector('#exit');
 // console.log(exitBu);
 
-startBu.addEventListener('click', () => {
+// startBu.addEventListener('click', () => {
+//   let div = document.createElement('div');
+//   div.innerHTML = '<a href="./public/html/mainPage.html">게임시작</a>';
+//   root.appendChild(div);
+// });
+
+exitBu.addEventListener('click', () => {
+  let box = document.createElement('div');
   let div = document.createElement('div');
-  div.innerHTML = '<a href="./public/html/mainPage.html">게임시작</a>';
-  root.appendChild(div);
 });
