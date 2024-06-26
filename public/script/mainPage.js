f4e98cb2a165464e514c45e4a12325471d20452d
Nicknul <jea7730@gmail.com>
2024-06-26 11:09:47 +0900

    브라우저 오른쪽에 마우스가 있다면 장바구니 구역이 나타나게 처리

--- public/script/mainPage.js
+++ public/script/mainPage.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const snappersImage = document.querySelector('.clickable-image');
+  const snappersImage = document.querySelector('img[alt="Snappers Image"]');
   const itemContainer = document.querySelector('.item-container');
+  const sellContainer = document.querySelector('.sell');
 
   snappersImage.addEventListener('click', () => {
     itemContainer.classList.toggle('active');
+  });
+
+  document.addEventListener('mousemove', (event) => {
+    const windowWidth = window.innerWidth;
+    const threshold = 20; // 마우스 위치가 오른쪽 끝에서 20px 이내일 때
+
+    if (event.clientX > windowWidth - threshold) {
+      sellContainer.style.transform = 'translateX(0)';
+      sellContainer.style.opacity = '1';
+    } else {
+      sellContainer.style.transform = 'translateX(100%)';
+      sellContainer.style.opacity = '0';
+    }
   });
 });
