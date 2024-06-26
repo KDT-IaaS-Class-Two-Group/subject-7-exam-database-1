6a431fe892a5e12917de91934e6e868dff9288df
Nicknul <jea7730@gmail.com>
2024-06-26 16:17:36 +0900

    test

--- public/script/mainPage.js
+++ public/script/mainPage.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const snappersImage = document.querySelector('img[alt="Snappers Image"]');
   const itemContainer = document.querySelector('.item-container');
-  const sellContainer = document.querySelector('.sell');
+  const historyContainer = document.querySelector('.history');
 
   snappersImage.addEventListener('click', () => {
     itemContainer.classList.toggle('active');
@@ -12,11 +12,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const threshold = 20; // 마우스 위치가 오른쪽 끝에서 20px 이내일 때
 
     if (event.clientX > windowWidth - threshold) {
-      sellContainer.style.transform = 'translateX(0)';
-      sellContainer.style.opacity = '1';
+      historyContainer.style.transform = 'translateX(0)';
+      historyContainer.style.opacity = '1';
     } else {
-      sellContainer.style.transform = 'translateX(100%)';
-      sellContainer.style.opacity = '0';
+      historyContainer.style.transform = 'translateX(100%)';
+      historyContainer.style.opacity = '0';
     }
   });
 });
