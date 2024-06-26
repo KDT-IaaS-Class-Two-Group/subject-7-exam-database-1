432de469acf64bf3ebd4b3347f3ed07a5e30df62
Nicknul <jea7730@gmail.com>
2024-06-26 17:16:03 +0900

    장바구니가 나타나면 아이템 컨테이너가 줄어들게 변경

--- public/script/main.js
+++ public/script/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const snappersImage = document.querySelector('img[alt="Snappers Image"]');
+  const snappersImage = document.querySelector('.snappers-image');
   const itemContainer = document.querySelector('.item-container');
   const historyContainer = document.querySelector('.history');
 
@@ -14,9 +14,11 @@ document.addEventListener('DOMContentLoaded', () => {
     if (event.clientX > windowWidth - threshold) {
       historyContainer.style.transform = 'translateX(0)';
       historyContainer.style.opacity = '1';
+      itemContainer.style.width = `calc(100% - 20%)`; // history의 너비를 제외한 나머지 너비
     } else {
       historyContainer.style.transform = 'translateX(100%)';
       historyContainer.style.opacity = '0';
+      itemContainer.style.width = '100%'; // history가 숨겨지면 전체 너비 사용
     }
   });
 });
