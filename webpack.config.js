5309063c8d39bad9c4fcb01c319551f818eb751d
KimInteger <kjs20151240@gmail.com>
2024-07-09 22:54:59 +0900

    chore_김정수 : webpack.config.js 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,45 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/app.ts',
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
   },
+  mode: 'production',
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(js|jsx|ts|tsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext][query]',
+        },
       },
     ],
   },
   resolve: {
-    extensions: ['.css', '.js', '.jsx', '.ts', '.tsx', '.png'],
+    extensions: ['.tsx', '.ts', '.js'],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body',
+      scriptLoading: 'blocking',
+      minify: false, // 필요에 따라 HTML을 압축하려면 true로 설정하세요
+    }),
+  ],
+  devtool: false,
 };
